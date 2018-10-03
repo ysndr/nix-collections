b298d1c9817a0b9fd427c89e12624411d73ee1c9
Yannik Sander <me@ysndr.de>
2018-10-03 13:03:48 +0200

    Pandoc: add common collections

--- pandoc-env/default.nix
+++ pandoc-env/default.nix
@@ -4,15 +4,20 @@
 
 with pkgs;
 let
-  latex = pkgs.texlive.combine {
-    inherit (texlive) scheme-basic;
+  latex = texlive.combine {
+    inherit (texlive) scheme-small
+    collection-langgerman
+    collection-latexextra
+    collection-mathscience ;
+    #isodate substr lipsum nonfloat supertabular;
   };
 
 
   # env-python-packages = packages: [ panflute ] ;
   # python = python3.withPackages env-python-packages;
 
-  nixpkgs = with pkgs; [
+  nixpkgs = [
+
     # nodejs
     vscode
     librsvg
@@ -28,7 +33,7 @@ let
 
   inputs = nixpkgs ++ [latex];
 
-  shell = pkgs.mkShell{name = "pandoc-shell"; buildInputs=inputs; shellHook="export SHELL=${pkgs.zsh}/bin/zsh "; };
+  shell = mkShell{name = "pandoc-shell"; buildInputs=inputs; shellHook="export SHELL=${pkgs.zsh}/bin/zsh "; };
   env = buildEnv {
     name = "pandoc-env";
     paths = inputs;
